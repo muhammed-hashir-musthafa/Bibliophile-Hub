508e5ec448b4860cfb869691916ca88de2852b3b
HashirMusthafa <hashirvaliyil8146@gmail.com>
2024-11-16 16:01:55 +0530

    feat: deploy back-end and connect it in api

--- Client Side/src/utils/api.js
+++ Client Side/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/books' 
+const API_URL = 'https://bibliophile-hub.onrender.com/books' 
 
 export const getBooks = async () => {
   return await axios.get(API_URL);
